ea45bbf4af901d2b5ca402b8b6babb16083e3299
Bumala <sospeter_kenya@hotmail.de>
2025-04-12 22:00:26 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -1,21 +1,56 @@
-<table style="border-spacing: 2px; width: 100%; border: 1px solid black;">
-  <thead>
-    <tr>
-      <th style="border: 1px solid black;">Header 1</th>
-      <th style="border: 1px solid black;">Header 2</th>
-      <th style="border: 1px solid black;">Header 3</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td style="border: 1px solid black;">Row 1, Col 1</td>
-      <td style="border: 1px solid black;">Row 1, Col 2</td>
-      <td style="border: 1px solid black;">Row 1, Col 3</td>
-    </tr>
-    <tr>
-      <td style="border: 1px solid black;">Row 2, Col 1</td>
-      <td style="border: 1px solid black;">Row 2, Col 2</td>
-      <td style="border: 1px solid black;">Row 2, Col 3</td>
-    </tr>
-  </tbody>
-</table>
+import streamlit as st
+import pandas as pd
+
+# Data definition
+data = [
+    ["Impact (What)", "Benefits", "Quality/Scope/Knowledge", "Time Efficiency", "Cost"],
+    [None, "Row 2, Col 2", "Row 2, Col 3", "Row 2, Col 4", None],
+    [None, "Row 3, Col 2", "Row 3, Col 3", "Row 3, Col 4", None],
+    [None, "Row 4, Col 2", "Row 4, Col 3", "Row 4, Col 4", None],
+    [None, "Row 5, Col 2", "Row 5, Col 3", "Row 5, Col 4", None],
+    ["Technology (How)", "Row 6, Col 2", "Row 6, Col 3", "Row 6, Col 4", None],
+    [None, "Row 7, Col 2", "Row 7, Col 3", "Row 7, Col 4", None],
+    [None, "Row 8, Col 2", "Row 8, Col 3", "Row 8, Col 4", None],
+    [None, "Row 9, Col 2", "Row 9, Col 3", "Row 9, Col 4", None],
+    [None, "Row 10, Col 2", "Row 10, Col 3", "Row 10, Col 4", None],
+    ["Place (Where)", "Row 11, Col 2", "Row 11, Col 3", "Row 11, Col 4", None],
+    [None, "Row 12, Col 2", "Row 12, Col 3", "Row 12, Col 4", None],
+]
+
+df = pd.DataFrame(data)
+
+# Style generator for table cells
+def cell_style(col_index):
+    style = "text-align: left; padding: 5px; border: 1px solid #ddd;"
+    if col_index in [0, 1]:
+        style += " font-weight: bold;"
+    return style
+
+# Generate HTML table
+def generate_html_table(df):
+    # Removed 'border-collapse: collapse;' and added 'border-spacing'
+    html = "<table style='border-spacing: 2px; width: 100%; border: 1px solid black;'>"
+    for i, row in df.iterrows():
+        html += "<tr>"
+        for j, val in enumerate(row):
+            style = cell_style(j)
+
+            if j == 0:  # Handle merged cells
+                if i == 0 and val == "Impact (What)":
+                    html += f"<td rowspan='5' style='{style}'>{val}</td>"
+                elif i == 5 and val == "Technology (How)":
+                    html += f"<td rowspan='5' style='{style}'>{val}</td>"
+                elif i == 10 and val == "Place (Where)":
+                    html += f"<td rowspan='2' style='{style}'>{val}</td>"
+                elif val is not None:
+                    html += f"<td style='{style}'>{val}</td>"
+            elif val is not None:
+                html += f"<td style='{style}'>{val}</td>"
+            else:
+                html += f"<td style='{cell_style(j)}'></td>"
+        html += "</tr>"
+    html += "</table>"
+    return html
+
+# Display table in Streamlit
+st.write(generate_html_table(df), unsafe_allow_html=True)
