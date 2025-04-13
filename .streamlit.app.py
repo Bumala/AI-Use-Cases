e4061b562e7cd41523ea2305e8f6a9ac9df307b5
Bumala <sospeter_kenya@hotmail.de>
2025-04-13 14:24:59 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -19,57 +19,68 @@ data = [
 
 df = pd.DataFrame(data)
 
-# Generate HTML table
 def generate_html_table(df):
-    # Set universal dimensions
-    cell_width = 150  # in px
-    cell_height = "50px"
+    # Dimensions
+    base_width = 150  # unit column width
+    first_col_width = 160
+    second_col_width = 220
+    cell_height = "60px"
 
-    html = "<table style='border-spacing: 0; width: 100%; border-collapse: collapse; table-layout: fixed;'>"
+    html = """
+    <table style='border-spacing: 0; border-collapse: collapse; width: 100%;'>
+    """
 
     for i, row in df.iterrows():
         html += "<tr>"
         for j, val in enumerate(row):
             if pd.notna(val):
 
-                # Merge logic
+                # First column: merged rows
                 if j == 0 and i == 0:
-                    html += f"<td rowspan='5' style='text-align: left; padding: 10px; width: {cell_width}px; height: {cell_height}; border: 1px solid #ccc;'>{val}</td>"
+                    html += f"<td rowspan='5' style='width: {first_col_width}px; height: {cell_height}; padding: 10px; border: 1px solid #ccc; vertical-align: top;'>{val}</td>"
                 elif j == 0 and i == 5:
-                    html += f"<td rowspan='5' style='text-align: left; padding: 10px; width: {cell_width}px; height: {cell_height}; border: 1px solid #ccc;'>{val}</td>"
+                    html += f"<td rowspan='5' style='width: {first_col_width}px; height: {cell_height}; padding: 10px; border: 1px solid #ccc; vertical-align: top;'>{val}</td>"
                 elif j == 0 and i == 10:
-                    html += f"<td rowspan='2' style='text-align: left; padding: 10px; width: {cell_width}px; height: {cell_height}; border: 1px solid #ccc;'>{val}</td>"
-                elif j == 0 and (i < 5 or (5 < i < 10) or i == 11):
+                    html += f"<td rowspan='2' style='width: {first_col_width}px; height: {cell_height}; padding: 10px; border: 1px solid #ccc; vertical-align: top;'>{val}</td>"
+                elif j == 0:
                     continue
 
-                # Colspan rules
-                colspan_2 = {
-                    (0, 2), (0, 3), (0, 4),
-                    (1, 4), (1, 5),
-                    (2, 4), (2, 5),
-                    (3, 2), (3, 3), (3, 4),
-                    (5, 2), (5, 3), (5, 4),
-                    (7, 4), (7, 5),
-                    (8, 6),
-                    (10, 2), (10, 3), (10, 4),
-                    (11, 2), (11, 3), (11, 4)
-                }
+                # Second column: always one cell per row
+                elif j == 1:
+                    html += f"<td style='width: {second_col_width}px; height: {cell_height}; padding: 10px; border: 1px solid #ccc; vertical-align: top;'>{val}</td>"
 
-                colspan_3 = {
-                    (4, 2), (4, 3)
-                }
-
-                if (i, j) in colspan_2:
-                    html += f"<td colspan='2' style='text-align: left; padding: 10px; width: {cell_width*2}px; height: {cell_height}; border: 1px solid #ccc;'>{val}</td>"
-                elif (i, j) in colspan_3:
-                    html += f"<td colspan='3' style='text-align: left; padding: 10px; width: {cell_width*3}px; height: {cell_height}; border: 1px solid #ccc;'>{val}</td>"
+                # All other cells (third column onward)
                 else:
-                    html += f"<td style='text-align: left; padding: 10px; width: {cell_width}px; height: {cell_height}; border: 1px solid #ccc;'>{val}</td>"
+                    # Define colspans
+                    colspan_2 = {
+                        (0, 2), (0, 3), (0, 4),
+                        (1, 4), (1, 5),
+                        (2, 4), (2, 5),
+                        (3, 2), (3, 3), (3, 4),
+                        (5, 2), (5, 3), (5, 4),
+                        (7, 4), (7, 5),
+                        (8, 6),
+                        (10, 2), (10, 3), (10, 4),
+                        (11, 2), (11, 3), (11, 4)
+                    }
+
+                    colspan_3 = {
+                        (4, 2), (4, 3)
+                    }
+
+                    if (i, j) in colspan_3:
+                        width = base_width * 3
+                        html += f"<td colspan='3' style='width: {width}px; height: {cell_height}; padding: 10px; border: 1px solid #ccc; vertical-align: top;'>{val}</td>"
+                    elif (i, j) in colspan_2:
+                        width = base_width * 2
+                        html += f"<td colspan='2' style='width: {width}px; height: {cell_height}; padding: 10px; border: 1px solid #ccc; vertical-align: top;'>{val}</td>"
+                    else:
+                        html += f"<td style='width: {base_width}px; height: {cell_height}; padding: 10px; border: 1px solid #ccc; vertical-align: top;'>{val}</td>"
         html += "</tr>"
 
     html += "</table>"
     return html
 
 # Display in Streamlit
-st.markdown("### Morphological Box with Fixed Cell Size")
+st.markdown("### Morphological Box (Aligned, Uniform Cells with Colspans)")
 st.markdown(generate_html_table(df), unsafe_allow_html=True)
