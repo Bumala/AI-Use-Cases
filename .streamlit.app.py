a0cc014255ff7b2157fc26a6b7c448510fbf48c2
Bumala <sospeter_kenya@hotmail.de>
2025-04-13 12:31:36 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -27,28 +27,6 @@ def generate_html_table(df):
     <table style='border-spacing: 0; width: 100%; border-collapse: collapse;'>
     """
 
-    # Generate rows dynamically
-    for i, row in df.iterrows():
-        html += "<tr>"
-        for j, val in enumerate(row):
-            if pd.notna(val):  # Only add non-empty cells
-                if j == 0 and i == 0:  # Merge rows 1 to 5 in the first column
-                    html += f"<td rowspan='5' style='text-align: left; padding: 10px;'>{val}</td>"
-                elif j == 0 and i == 5:  # Merge rows 6 to 10 in the first column
-                    html += f"<td rowspan='5' style='text-align: left; padding: 10px;'>{val}</td>"
-                elif j == 0 and i == 10:  # Merge rows 11 and 12 in the first column
-                    html += f"<td rowspan='2' style='text-align: left; padding: 10px;'>{val}</td>"
-                elif j == 0 and i < 5:  # Skip rows 2 to 5 in the first column
-                    continue
-                elif j == 0 and 5 < i < 10:  # Skip rows 7 to 10 in the first column
-                    continue
-                elif j == 0 and i == 11:  # Skip row 12 in the first column
-                    continue
-                elif i == 0 and j == 3:  # Make the fourth column in the first row wider
-                    html += f"<td colspan='2' style='text-align: left; padding: 10px;'>{val}</td>"
-                else:  # Regular cells
-                    html += f"<td style='text-align: left; padding: 10px;'>{val}</td>"
-        html += "</tr>"
 
     html += "</table>"
     return html
