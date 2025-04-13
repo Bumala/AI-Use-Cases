43fce5d0ddf1b0773e54b67d602042bc4ff01b36
Bumala <sospeter_kenya@hotmail.de>
2025-04-13 08:35:31 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -22,7 +22,6 @@ df = pd.DataFrame(data)
 
 # Generate HTML table with flexible row lengths and adjusted column widths
 def generate_html_table(df):
-    num_rows = len(df)
     html = f"<table style='border-spacing: 2px; width: 100%; border-collapse: collapse; border: 1px solid black; aspect-ratio: 1 / 1;'>"
     for i, row in df.iterrows():
         non_empty_cells = row.dropna().size
@@ -30,23 +29,18 @@ def generate_html_table(df):
         for j, val in enumerate(row):
             if pd.notna(val):
                 style = f"text-align: left; padding: 10px; border: 1px solid #ddd;"
-                if non_empty_cells == 2:
-                    if j == 0:
-                        html += f"<td style='{style} width: 50%;' colspan='1'>{val}</td>"
-                    elif j == 1:
-                        html += f"<td style='{style} width: 50%;' colspan='{df.iloc[i].size - 1}'>{val}</td>"
-                elif non_empty_cells > 2:
-                    if j == 0:
-                        html += f"<td rowspan='{df.iloc[i].isnull().sum() + 1 if i in [0, 5, 10] else 1}' style='{style}'>{val}</td>"
-                    elif pd.notna(df.iloc[i, j-1]) or j == 1: # Ensure we don't add a new cell if the previous was merged
-                        # Calculate dynamic width for columns from the third onwards
-                        if j >= 1:
-                            num_remaining_cols = non_empty_cells - (1 if i not in [0, 5, 10] else 1) # Adjust for the first merged column
-                            if num_remaining_cols > 0:
-                                width_percent = 100 / non_empty_cells
-                                html += f"<td style='{style} width: {width_percent}%;'>{val}</td>"
-                elif non_empty_cells == 1:
-                    html += f"<td style='{style} width: 100%;' colspan='{df.iloc[i].size}'>{val}</td>"
+                if j == 0:
+                    rowspan = df.iloc[i:].apply(lambda r: r.iloc[0] is None).take_while(lambda x: x).size + 1 if i in [0, 5, 10] else 1
+                    html += f"<td rowspan='{rowspan}' style='{style}'>{val}</td>"
+                elif pd.notna(df.iloc[i, j-1]) or j == 1:
+                    width_percent = f'{100 / (non_empty_cells - (1 if i not in [0, 5, 10] and j > 0 else 0)) if non_empty_cells > 1 else 100}%'
+                    colspan = 1
+                    if non_empty_cells == 2:
+                        colspan = df.iloc[i].notna().sum() -1 if j == 1 else 1
+
+                    html += f"<td style='{style} width: {width_percent};' colspan='{colspan}'>{val}</td>"
+            elif non_empty_cells > 0 and j > 0 and pd.isna(df.iloc[i, j-1]):
+                pass # Don't add empty cells if the previous was a value in a shorter row
         html += "</tr>"
     html += "</table>"
     return html
