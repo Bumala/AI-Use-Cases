077acf85a7544789e920b456ffcb93042f9c1388
Bumala <sospeter_kenya@hotmail.de>
2025-04-13 06:43:23 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -4,17 +4,17 @@ import pandas as pd
 # Data definition
 data = [
     ["Impact (What)", "Benefits", "Quality/Scope/Knowledge", "Time Efficiency", "Cost"],
-    [None, "Row 2, Col 2"],
-    [None, "Row 3, Col 2", "Row 3, Col 3"],
-    [None, "Row 4, Col 2"],
-    [None, "bbbbb"],
-    ["Technology (How)", "Row 6, Col 2", "Row 6, Col 3", "Row 6, Col 4"],
-    [None, "Row 7, Col 2", "Row 7, Col 3"],
-    [None, "Row 8, Col 2"],
-    [None,"aaaa"],
-    [None,"cccccc"],
-    ["Place (Where)", "Row 11, Col 2", "Row 11, Col 3", "Row 11, Col 4"],
-    [None, "Row 12, Col 2", "Row 12, Col 3"],
+    [None, "Focus within Business Model Navigator", "Customer Segments", "Value Proposition", "Value Chain", "Revenue Model"],
+    [None, "Innovation Type", "Incremental", "Radical", "Sustaining", "Disruptive"],
+    [None, "Aim", "Product Innovation", "Process Innovation", "Business Model Innovation"],
+    [None, "Ambidexterity", "Exploration", "Exploitation"],
+    ["Technology (How)", "AI Role", "Automaton", "Assistant", "Partner"],
+    [None, "AI Concepts", "Machine Learning", Deep Learning, "Artificial Neural Networks", "Natural Language Processing", "Computer Vision", "Robotics"],
+    [None, "Analytics Focus", "Descriptive", "Diagnostic", "Predictive", "Prescriptive" ],
+    [None,"Analytics Problem", "Description/Summary", "Clustering", "Classification", "Dependency Analysis", "Regression"],
+    [None,"Data Type", "Customer Data", "Machine Data", "Business Data (Internal Data)", "Market Data", "Public & Regulatory Data", "Synthetic Data"],
+    ["Place (Where)", "Innovation Phase", "Front End", "Development", "Market Introduction"],
+    [None, "R&D", "Manufacturing", "Marketing & Sales", "Customer Service"],
   
 ]
 
