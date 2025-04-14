2bded0f6fc41030eab5badbadd229285d8f08506
Bumala <sospeter_kenya@hotmail.de>
2025-04-14 10:07:01 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -92,10 +92,7 @@ def generate_html_table(df):
              #elif i == 9 and 2 <= j <= 7:  
                 #html += f"<td style='{style(base_cell_width, bold=True)} background-color: #f1fbfe; border: 1px solid #000000; border-bottom: 3px solid #000000;'>{val}</td>"
 
-if i == 9 and j == 1:  # Specific cell
-    html += f"<td style='{style(base_cell_width, bold=True)} background-color: #94dcf8;'>{val}</td>"
-elif i == 9 and 2 <= j <= 7:  # Apply black 3px bottom border to cells in row 9, columns 1 to 7
-    html += f"<td style='{style(base_cell_width, bold=True)} background-color: #f1fbfe; border: 1px solid #000000; border-bottom: 3px solid #000000;'>{val}</td>"
+
 elif j == 1:
     html += f"<td style='{style(second_col_width, bold=True)} background-color: #94dcf8;'>{val}</td>"
 elif (i, j) in colspan_3:
