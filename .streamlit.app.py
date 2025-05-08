2e766627bb02c8c3c4211fc0c22eba43efc242a1
Bumala <sospeter_kenya@hotmail.de>
2025-05-08 05:46:46 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -63,7 +63,9 @@ def generate_html_table(df):
                 
             #first row format
             if i == 0:
-    html += f"<td style='{style(first_col_width, bold=True)} background-color: #E8E8E8; border-bottom: 3px solid #000000;'>{val}</td>"
+                html += f"<td style='{style(first_col_width, bold=True)} background-color: #E8E8E8; border-bottom: 3px solid #000000;'>{val}</td>"
+            else:
+                continue
             
             #first column format, column 0
             if j == 0:
