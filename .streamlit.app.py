86de835ac851b05f1e1a7e9a4887d62d1dd68d03
Bumala <sospeter_kenya@hotmail.de>
2025-05-08 06:44:41 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -105,7 +105,7 @@ def generate_html_table(df):
                 html += f"<td colspan='3' style='{style(base_cell_width * 3)} background-color: #f1fbfe; border: 1px solid #000000; border-bottom: 3px solid #000000;'>{val}</td>"
             elif (i, j) in colspan_2:
                 html += f"<td colspan='2' style='{style(base_cell_width * 2)} background-color: #f1fbfe; border: 1px solid #000000;'>{val}</td>"
-            elif (i, j) in colspan_2:
+            elif (i, j) in colspan_6:
                 html += f"<td colspan='6' style='{style(base_cell_width * 6)} background-color: #E8E8E8; border-bottom: 3px solid #000000;'>{val}</td>"
             
 
