dd770e00e82f13d5e9d44f207c95beca206e8f76
Bumala <sospeter_kenya@hotmail.de>
2025-05-16 08:13:22 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -672,6 +672,7 @@ if top_use_case:
         title_text="Significance Level",
         range=[0, 2],
         title_font=dict(color='black'),
+        tickfont=dict(color='black'),
         
     )
     fig.update_xaxes(
@@ -679,7 +680,8 @@ if top_use_case:
     automargin=True,
     title_standoff=30,  # Lower value brings title closer to axis
     tickangle=50,
-    title_font=dict(color='black')
+    title_font=dict(color='black'),
+    tickfont=dict(color='black'),
 )
     fig.update_layout(
     margin=dict(b=40)  # Adjust bottom margin (try 20-60)
