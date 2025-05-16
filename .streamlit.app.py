cb82e072eb2361f95a358dc2254ea42e0c16de5a
Bumala <sospeter_kenya@hotmail.de>
2025-05-16 07:45:57 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -672,8 +672,14 @@ if top_use_case:
         title_text="Significance Level",
         range=[0, 2]
     )
-    fig.update_xaxes(title_text="Attributes")
-
+    fig.update_xaxes(
+    title_text="Attributes",
+    automargin=True,
+    title_standoff=5  # Lower value brings title closer to axis
+)
+fig.update_layout(
+    margin=dict(b=40)  # Adjust bottom margin (try 20-60)
+)
     st.plotly_chart(fig, use_container_width=True)
 
 
