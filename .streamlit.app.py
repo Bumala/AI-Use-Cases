eb4c1da9ad4a3dcf72b63d5de2f5afb31cd98110
Bumala <sospeter_kenya@hotmail.de>
2025-05-16 13:04:03 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -475,7 +475,7 @@ else:
 #------------------------------------------------------------------------ Top use case graph display ------------------------------------------------------------------------------
 
 st.markdown(
-    "<h3 style='font-size:18px; font-weight:700; margin-bottom:0; margin-top:10; text-align:center;'>Significance levels of attributes for the most relevant AI use case in automotive, based on the user's selection</h3>", 
+    "<h3 style='font-size:18px; font-weight:700; margin-bottom:0; margin-top:1e; text-align:center;'>Significance levels of attributes for the most relevant AI use case in automotive, based on the user's selection</h3>", 
     unsafe_allow_html=True
 )
 
