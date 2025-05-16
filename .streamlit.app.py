03a9fcd2600143a9a29c0cda20d68746a6f9f32f
Bumala <sospeter_kenya@hotmail.de>
2025-05-16 07:56:57 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -675,7 +675,7 @@ if top_use_case:
     fig.update_xaxes(
     title_text="Attributes",
     automargin=True,
-    title_standoff=5  # Lower value brings title closer to axis
+    title_standoff=5,  # Lower value brings title closer to axis
     tickangle=20
 )
     fig.update_layout(
