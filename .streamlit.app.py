fefee6e0f14de58a80e9fb99531dec200c09d9c4
Bumala <sospeter_kenya@hotmail.de>
2025-05-18 14:55:50 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -847,8 +847,8 @@ ctx.beginPath();
 // Bell curve (top)
 ctx.moveTo(points.bellStart.x, points.bellStart.y);
 ctx.bezierCurveTo(
-points.bellStart.x + w * 0.2, points.bellStart.y+25,
-points.bellEnd.x - w * 0.05, points.bellEnd.y-10,
+points.bellStart.x + w * 0.1, points.bellStart.y + 25,
+points.bellEnd.x - w * 0.1, points.bellEnd.y - 10,
 points.bellEnd.x, points.bellEnd.y
 );
  
@@ -863,8 +863,8 @@ ctx.lineTo(points.bellBottomEnd.x, points.bellBottomEnd.y);
  
 // Bottom bell curve (mirror of top)
 ctx.bezierCurveTo(
-points.bellBottomEnd.x - w * 0.2, points.bellBottomEnd.y+10,
-points.bellBottomStart.x + w * 0.05, points.bellBottomStart.y-25,
+points.bellBottomEnd.x - w * 0.1, points.bellBottomEnd.y + 10,
+points.bellBottomStart.x + w * 0.1, points.bellBottomStart.y - 25,
 points.bellBottomStart.x, points.bellBottomStart.y
 );
  
