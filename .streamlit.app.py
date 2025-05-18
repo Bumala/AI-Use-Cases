41191b40e05b9b04547d62857489c573423b3243
Bumala <sospeter_kenya@hotmail.de>
2025-05-18 19:20:03 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -795,8 +795,8 @@ yMin: innerFunnelPoints.bellStart.y, yMax: innerFunnelPoints.bellBottomStart.y}
 const marketIntroOuterBounds = {
 xMin: 900,
 xMax: 1500,
-yMin: h/2 - 60,
-yMax: h/2 + 60
+yMin: h/2 - 100,
+yMax: h/2 + 100
 };
  
 let sectionDots = [];
