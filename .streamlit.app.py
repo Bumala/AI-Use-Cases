9e5c1e87dd93976ea4bafe2806254b9454995722
Bumala <sospeter_kenya@hotmail.de>
2025-05-19 23:20:44 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -1184,7 +1184,7 @@ if selected_attributes:
        <div style="margin-top: 1em;">
            <label style="font-weight: 700; color: #000;"> Other relevant AI Use Cases</label><br>
            <div style="
-               background-color: #F8CAA0;
+               background-color: #f7cbb5;
                padding: 10px;
                border-radius: 8px;
                border: 1px solid #000;
