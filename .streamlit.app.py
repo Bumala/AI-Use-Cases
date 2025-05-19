e435ef5f3b8bba4f99c87ecedb07a0a7b7449206
Bumala <sospeter_kenya@hotmail.de>
2025-05-19 12:23:18 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -190,19 +190,21 @@ function drawTrumpetFunnel(points, color) {
   ctx.beginPath();
   ctx.moveTo(points.bellStart.x, points.bellStart.y);
   ctx.bezierCurveTo(
-  points.bellStart.x + w * 0.15, points.bellStart.y + 40,
+  points.bellStart.x + w * 0.25, points.bellStart.y - 50,
   points.bellEnd.x - w * 0.05, points.bellEnd.y - 30,
   points.bellEnd.x, points.bellEnd.y
 );
+
 
   ctx.lineTo(points.tubeEnd.x, points.tubeEnd.y);
   ctx.lineTo(points.mouthBottom.x, points.mouthBottom.y);
   ctx.lineTo(points.bellBottomEnd.x, points.bellBottomEnd.y);
   ctx.bezierCurveTo(
   points.bellBottomEnd.x - w * 0.05, points.bellBottomEnd.y + 30,
-  points.bellBottomStart.x + w * 0.15, points.bellBottomStart.y - 40,
+  points.bellBottomStart.x + w * 0.25, points.bellBottomStart.y + 50,
   points.bellBottomStart.x, points.bellBottomStart.y
 );
+
 
   ctx.closePath();
   ctx.fill();
