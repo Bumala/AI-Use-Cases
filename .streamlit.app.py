0d87d500293c46d8cbac781c4c4bc7d05fdc7cf5
Bumala <sospeter_kenya@hotmail.de>
2025-05-19 16:17:28 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -412,7 +412,7 @@ import streamlit as st
 html_code = """
 <div style="display: flex; justify-content: center; gap: 400px; margin-top: 50px;">
     <p style="font-size: 18px; text-align: center; 
-              margin-left: -50px; margin-top: -30px;">
+              margin-left: -100px; margin-top: -40px;">
         AI in design
     </p>
     <p style="font-size: 18px; text-align: center;">
