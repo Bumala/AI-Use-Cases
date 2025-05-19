49bbb4e6bf2c411ad90fb1fca8658ec47d20d04b
Bumala <sospeter_kenya@hotmail.de>
2025-05-19 12:49:48 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -190,8 +190,8 @@ function drawTrumpetFunnel(points, color) {
   ctx.beginPath();
   ctx.moveTo(points.bellStart.x, points.bellStart.y);
   ctx.bezierCurveTo(
-  points.outerStart.x + w * 0.2, points.outerStart.y - h * 0.6,   // strong early flare
-  points.outerEnd.x - w * 0.1, points.outerEnd.y - h * 0.15,      // soft, aligned pull-in
+  points.outerStart.x + w * 0.2, points.outerStart.y - 450 * 0.6,   // strong early flare
+  points.outerEnd.x - w * 0.1, points.outerEnd.y - 450 * 0.15,      // soft, aligned pull-in
   points.outerEnd.x, points.outerEnd.y
 );
 
@@ -200,8 +200,8 @@ function drawTrumpetFunnel(points, color) {
   ctx.lineTo(points.mouthBottom.x, points.mouthBottom.y);
   ctx.lineTo(points.bellBottomEnd.x, points.bellBottomEnd.y);
   ctx.bezierCurveTo(
-  points.outerEnd.x - w * 0.1, points.outerEnd.y + h * 0.15,      // soft, aligned pull-in
-  points.outerStart.x + w * 0.2, points.outerStart.y + h * 0.6,   // strong early flare
+  points.outerEnd.x - w * 0.1, points.outerEnd.y + 450 * 0.15,      // soft, aligned pull-in
+  points.outerStart.x + w * 0.2, points.outerStart.y + 450 * 0.6,   // strong early flare
   points.outerStart.x, points.outerStart.y
 );
 
