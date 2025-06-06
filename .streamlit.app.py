e5600138b7e2b84425a544fced78e5b996d993eb
Bumala <sospeter_kenya@hotmail.de>
2025-06-06 20:10:33 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -381,7 +381,7 @@ components.html(html_code, height=500)
 #---------------------------------------------------------------------------------------------- Introduction -------------------------------------------------------------------------------------------------------------------
 
 
-st.markdown("<p style='font-weight: 700; font-size:18px;  margin-bottom:0; text-align:center;'>AI´s impact on the automotive innovation process: </p>", unsafe_allow_html=True)
+st.markdown("<p style='font-weight: 700; font-size:18px;  margin-bottom:0; text-align:center;'>AI's impact on the automotive innovation process: </p>", unsafe_allow_html=True)
 
 # Settings
 font_size = 30
@@ -405,16 +405,11 @@ html_code = f"""
 st.markdown(html_code, unsafe_allow_html=True)
 
 
-
-
-
-
-
 html_code = """
 <div style="display: flex; justify-content: center; gap: 100px; margin-top: 50px;">
     <p style="font-size: 18px; text-align: right; font-weight: 700; 
               margin-left: 50px; margin-top: -40px; white-space: nowrap">
-        Amplification of idea space
+        Expansion of idea space
     </p>
     <p style="font-size: 18px; text-align: center; margin-top: -40px; white-space: nowrap; font-weight: 700">
         Efficiency improvements in product development
