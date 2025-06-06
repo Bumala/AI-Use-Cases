2e8167bf9028b6d6e6362ba9dacdc8a11594e1a0
Bumala <sospeter_kenya@hotmail.de>
2025-06-06 21:12:43 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -381,7 +381,7 @@ components.html(html_code, height=500)
 st.markdown("<p style='font-weight: 700; font-size:20px; margin-bottom:10; text-align:center;'>AI's impact on the automotive innovation process: </p>", unsafe_allow_html=True)
 
 # Settings
-font_size = 50
+font_size = 30
 stretch_y = 4  # Stretch factor
 
 # HTML and CSS
