37926ec9cfb0e920ca51f8a26fdc8d4bd3850300
Bumala <sospeter_kenya@hotmail.de>
2025-06-07 06:15:08 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -597,6 +597,20 @@ data = [
 
 
 
+attributes = [
+ ["Category", "Dimension", "Attributes"],
+ ["Impact (What)", "Benefits", "Quality/Scope/Knowledge", "Time Efficiency", "Cost"],
+ [None, "Focus within Business Model Navigator", "Customer Segments", "Value Proposition", "Value Chain", "Revenue Model"],
+ [None, "Aim", "Product Innovation", "Process Innovation", "Business Model Innovation"],
+ [None, "Ambidexterity", "Exploration", "Exploitation"],
+ ["Technology (How)", "AI Role", "Automaton", "Helper", "Partner"],
+ [None, "AI Concepts", "Machine Learning", "Deep Learning", "Artificial Neural Networks", "Natural Language Processing", "Computer Vision", "Robotics"],
+ [None, "Analytics Focus", "Descriptive", "Diagnostic", "Predictive", "Prescriptive"],
+ [None, "Analytics Problem", "Description/ Summary", "Clustering", "Classification", "Dependency Analysis", "Regression"],
+ [None, "Data Type", "Customer Data", "Machine Data", "Business Data (Internal Data)", "Market Data", "Public & Regulatory Data", "Synthetic Data"],
+ ["Context (Where/When)", "Innovation Phase", "Front End", "Development", "Market Introduction"],
+ [None, "Department", "R&D", "Manufacturing", "Marketing & Sales", "Customer Service"],
+]
 
 
 
