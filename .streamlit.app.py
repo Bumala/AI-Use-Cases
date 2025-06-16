81cde2f8d4d666553ffc19504e9501d03a75e9fb
Bumala <sospeter_kenya@hotmail.de>
2025-06-16 02:39:13 +0200

    Update .streamlit.app.py

--- .streamlit.app.py
+++ .streamlit.app.py
@@ -13,7 +13,7 @@ st.set_page_config(layout="wide")
 #------------------------------------------------------------------------------------------------------------- Funnel image -------------------------------------------------------------------------------------------------------------------
 
 html_code = """
-<canvas id="funnelCanvas" width="1000" height="450" style="width: 1000px; height: 450px; background: white;"></canvas>
+<canvas id="funnelCanvas" width="1000" height="450" style="width: 100%; height: auto; background: white;"></canvas>
 
 
 <script>
